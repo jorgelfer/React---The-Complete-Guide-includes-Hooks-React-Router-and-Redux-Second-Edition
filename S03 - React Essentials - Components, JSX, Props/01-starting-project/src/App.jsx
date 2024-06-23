ae868cbe228ee89e87a97efb62fa291c6a11060e
jorgelfer <jorgeferram17@gmail.com>
2024-06-23 14:52:28 -0400

    forwards props

--- S03 - React Essentials - Components, JSX, Props/01-starting-project/src/App.jsx
+++ S03 - React Essentials - Components, JSX, Props/01-starting-project/src/App.jsx
@@ -1,35 +1,17 @@
-import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header.jsx";
-
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
 
-        </section>
-        <h2>Time to get started!</h2>
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
